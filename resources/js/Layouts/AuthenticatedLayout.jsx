2e0bc77ee7ea57180014e1f2648c7f010182a77c
ives visagie <ives.visagie@payfast.co.za>
2025-06-27 15:05:44 +0200

    WIP

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -19,7 +19,7 @@ export default function AuthenticatedLayout({ header, children }) {
                         <div className="flex">
                             <div className="flex shrink-0 items-center">
                                 <Link href="/dashboard">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <img className="w-16 h-16 object-cover border rounded" alt="Thumbnail" src="/images/EngeneerWorx_1.png"/>
                                 </Link>
                             </div>
 
